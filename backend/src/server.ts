cd627b44ff0e070fcf40d1144df3e3de7270a0fb
pxxja <poojabhambid2004@gmail.com>
2025-12-18 15:43:28 +0530

    mongoDb connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,13 +3,27 @@ import app from "./app";
 import "dotenv/config";
 
 import { initSocket } from "./socket/index";
+import prisma from "./utils/prisma"; 
 
 const server = http.createServer(app);
 
-initSocket(server);
+//  Check MongoDB connection
+async function startServer() {
+  try {
+    await prisma.$connect();
+    console.log(" MongoDB connected successfully");
 
-const PORT = process.env.PORT || 5000;
+    initSocket(server);
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
+      console.log(` Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(" MongoDB connection failed");
+    console.error(error);
+    process.exit(1); // stop server if DB fails
+  }
+}
+
+startServer();
