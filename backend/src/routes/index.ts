50f69634da51618ff95e3096510fcc7b74a70803
pxxja <poojabhambid2004@gmail.com>
2025-12-18 22:02:39 +0530

    add entry point for task

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import authRouter from "./auth.routes";
+import taskRouter from "./task.routes";
 
 const mainRouter=Router();
 
-mainRouter.use("/auth",authRouter)
+mainRouter.use("/auth",authRouter);
+mainRouter.use("/task",taskRouter)
 export default mainRouter;
