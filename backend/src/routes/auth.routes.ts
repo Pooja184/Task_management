c45785dccc9b3e5c1c3b5e3441aa873a22991120
pxxja <poojabhambid2004@gmail.com>
2025-12-19 00:41:30 +0530

    route for all -users

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCurrentUser, login, logout, register } from "../controllers/auth.controller";
+import { getAllUsers, getCurrentUser, login, logout, register } from "../controllers/auth.controller";
 import { protect } from "../middlewares/auth.middleware";
 
 
@@ -9,5 +9,6 @@ authRouter.post("/register",register);
 authRouter.post("/login",login)
 authRouter.post("/logout",logout)
 authRouter.get("/profile",protect,getCurrentUser)
+authRouter.get("/all-users",protect,getAllUsers)
 
 export default authRouter
