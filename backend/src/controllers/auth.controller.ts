78d03a560b88cd7e91040f4a7db707eaf38a2a41
pxxja <poojabhambid2004@gmail.com>
2025-12-18 15:45:05 +0530

    create register and login controller

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import prisma from "../utils/prisma";
+import { signToken } from "../utils/jwt";
+import { success } from "zod";
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+  
+    const existing = await prisma.user.findUnique({ where: { email } });
+    if (existing) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+  
+    const hashedPassword = await bcrypt.hash(password, 10);
+  
+    const user = await prisma.user.create({
+      data: { name, email, password: hashedPassword },
+    });
+  
+    res.status(201).json({ message: "User registered", user });
+  } catch (error:any) {
+    res.status(500).json({success:false,message:error.message})
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    // console.log(req.body)
+    const { email, password } = req.body;
+    console.log(email)
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+  
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+  
+    const token = signToken(user.id);
+  
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+  
+    res.json({ message: "Login successful", user });
+  } catch (error:any) {
+    res.status(500).json({success:false,message:error.message})
+    
+  }
+};
