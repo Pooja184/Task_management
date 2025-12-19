460398ca3295afe11a1f3bc2fa77d68cbcd4cd1b
pxxja <poojabhambid2004@gmail.com>
2025-12-19 16:18:59 +0530

    cookies error solve

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -51,7 +51,7 @@ export const register = async (req: Request, res: Response) => {
     // set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
@@ -88,7 +88,7 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
     });
 
