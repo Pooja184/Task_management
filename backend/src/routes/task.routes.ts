f24b250350bc9956af3c517d4fbb3d93d32573d8
pxxja <poojabhambid2004@gmail.com>
2025-12-19 12:12:18 +0530

    routes for tasks

--- backend/src/routes/task.routes.ts
+++ backend/src/routes/task.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { protect } from "../middlewares/auth.middleware";
-import { createTask, deleteTask, getMyTasks, getTasks, updateTask } from "../controllers/task.controller";
+import { createTask, deleteTask, getAssignedToMeTasks, getMyTasks, getTasks, updateTask, updateTaskStatus } from "../controllers/task.controller";
 
 const taskRouter=Router();
 
@@ -9,5 +9,6 @@ taskRouter.get("/get-tasks",protect,getTasks);
 taskRouter.put("/:id",protect,updateTask)
 taskRouter.delete("/:id",protect,deleteTask)
 taskRouter.get("/my-tasks",protect,getMyTasks)
-
+taskRouter.patch("/:taskId/status",protect,updateTaskStatus)
+taskRouter.get("/assigned-to-me",protect,getAssignedToMeTasks)
 export default taskRouter;
